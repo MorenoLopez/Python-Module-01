d0eaacae2c5d96b067f915e2dd7ff008ec55944c
4n0ny_m0 <127933255+MorenoLopez@users.noreply.github.com>
2026-07-13 09:58:05 +0000

    ex1

---
+++ ex1/ft_garden_data.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# ########################################################################### #
+#   shebang: 1                                                                #
+#                                                          :::      ::::::::  #
+#   ft_garden_data.py                                    :+:      :+:    :+:  #
+#                                                      +:+ +:+         +:+    #
+#   By: horarivo <horarivo@student.42antananarivo.   +#+  +:+       +#+       #
+#                                                  +#+#+#+#+#+   +#+          #
+#   Created: 2026/04/14 12:13:48 by horarivo            #+#    #+#            #
+#   Updated: 2026/06/04 20:06:53 by horarivo           ###   ########.fr      #
+#                                                                             #
+# ########################################################################### #
+
+class Plant:
+    def __init__(self, name: str, height: float, age: int) -> None:
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def show(self) -> None:
+        print(f"{self.name}: {round(self.height, 1)}cm, {self.age} days old")
+
+
+if __name__ == "__main__":
+    rose = Plant("Rose", 25.0, 30)
+    sunflower = Plant("Sunflower", 80.0, 45)
+    cactus = Plant("Cactus", 15.0, 120)
+    print("=== Garden Plant Registry ===")
+    rose.show()
+    sunflower.show()
+    cactus.show()
