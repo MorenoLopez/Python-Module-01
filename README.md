ff683223c6b1dfb8ed6ba3fe82e00a9fe4a950bc
4n0ny_m0 <127933255+MorenoLopez@users.noreply.github.com>
2026-07-13 09:57:50 +0000

    ex0

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Python-Module-01
+
+## Description
+
+Learn Object-Oriented Programming by accidentally becoming a digital gardener - because apparently, the best way to understand classes is to grow virtual plants and realize you've been doing OOP all along! 🌱💻
---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# ########################################################################### #
+#   shebang: 1                                                                #
+#                                                          :::      ::::::::  #
+#   ft_garden_intro.py                                   :+:      :+:    :+:  #
+#                                                      +:+ +:+         +:+    #
+#   By: horarivo <horarivo@student.42antananarivo.   +#+  +:+       +#+       #
+#                                                  +#+#+#+#+#+   +#+          #
+#   Created: 2026/04/13 22:59:57 by horarivo            #+#    #+#            #
+#   Updated: 2026/04/14 12:11:59 by horarivo           ###   ########.fr      #
+#                                                                             #
+# ########################################################################### #
+
+if __name__ == "__main__":
+    name = "Rose"
+    height = 25
+    age = 30
+    print("=== Welcome to My Garden ===\n"
+          f"Plant: {name}\nHeight: {height}cm\nAge: {age} days"
+          "\n\n=== End of Program ==="
+          )
